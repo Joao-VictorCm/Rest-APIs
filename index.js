bc37484a3091f6d833aabd019f22dea8b2ebb73f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-10-24 22:31:45 -0300

    Concluida a task do metodo PUT

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Adicione seu próprio token ao portador da lição anterior.
-const yourBearerToken = "0035a9c5-8111-4b9f-af74-eb38d93f3cf7";
+const yourBearerToken = "51db1823-5df7-464f-9a3d-deb747618329";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,7 +27,7 @@ app.get("/", (req, res) => {
 app.post("/get-secret", async (req, res) => {
   const searchId = req.body.id;
   try {
-    const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+    const result = await axios.get(API_URL+"/secrets/"+searchId, config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
@@ -46,7 +47,7 @@ app.post("/post-secret", async (req, res) => {
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios para COLOCAR os dados de req.body nos servidores API de segredos.
-  const result = await axios.post(API_URL+"/secrets/"+ searchId, req.body, config);
+  const result = await axios.put(API_URL+"/secrets/"+searchId, req.body, config);
   res.render("index.ejs", { content: JSON.stringify(result.data) })
 });
 
